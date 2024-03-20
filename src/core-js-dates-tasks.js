4d4d79a5a66ca2480a5682262a6faa5d7d0db5d1
svetsitkh <svetsitkh@gmail.com>
2024-03-21 00:57:50 +0200

    feat: implement solution for getCountDaysInMonth, getCountDaysOnPeriod and isDateInPeriod tasks

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -78,8 +78,8 @@ function getNextFriday(date) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  return new Date(year, month, 0).getDate();
 }
 
 /**
@@ -93,8 +93,12 @@ function getCountDaysInMonth(/* month, year */) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  return (
+    Math.floor(
+      (new Date(dateEnd) - new Date(dateStart)) / (1000 * 60 * 60 * 24)
+    ) + 1
+  );
 }
 
 /**
@@ -114,8 +118,11 @@ function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  const dateToCheck = new Date(date);
+  return (
+    dateToCheck >= new Date(period.start) && dateToCheck <= new Date(period.end)
+  );
 }
 
 /**
